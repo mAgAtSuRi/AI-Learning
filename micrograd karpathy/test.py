ebc15343aabd7013405b82d0475a25a99ab3a9ea
mAgAtSuRi <tristan.latuilliere@gmail.com>
2026-09-11 17:19:21 +0200

    network with tensor

--- micrograd karpathy/test.py
+++ micrograd karpathy/test.py
@@ -1,5 +1,7 @@
 from graphviz import Digraph
 import math
+import torch
+import random
 
 class Value:
 	def __init__(self, data, _children=(), _op='', label = ''):
@@ -122,21 +124,21 @@ def draw_dot(root):
 # 	draw first graph
 # draw_dot(L).render('graph', format='svg')
 
-x1 = Value(2.0, label='x1')
-x2 = Value(0.0, label='x2')
-w1 = Value(-3.0, label='w1')
-w2 = Value(1.0, label='w2')
-b = Value(6.8813735870195432, label='b')
-x1w1 = x1 * w1; x1w1.label = 'x1w1'
-x2w2 = x2 * w2; x2w2.label = 'x2w2'
-x1w1x2w2 = x1w1 + x2w2; x1w1x2w2.label = 'x1w1x2w2'
-n = x1w1x2w2 + b; n.label = 'n'
+# x1 = Value(2.0, label='x1')
+# x2 = Value(0.0, label='x2')
+# w1 = Value(-3.0, label='w1')
+# w2 = Value(1.0, label='w2')
+# b = Value(6.8813735870195432, label='b')
+# x1w1 = x1 * w1; x1w1.label = 'x1w1'
+# x2w2 = x2 * w2; x2w2.label = 'x2w2'
+# x1w1x2w2 = x1w1 + x2w2; x1w1x2w2.label = 'x1w1x2w2'
+# n = x1w1x2w2 + b; n.label = 'n'
 # o = n.tanh(); o.label = 'o'
 # Other expression of o
-e = (2 * n).exp()
-o = (e - 1) / (e + 1)
-# Automatically 
-o.backward()
+# e = (2 * n).exp()
+# o = (e - 1) / (e + 1)
+# # Automatically 
+# o.backward()
 
 # Semi Automatically
 # o.grad = 1.0
@@ -158,4 +160,65 @@ o.backward()
 # n.grad = 0.5
 # o.grad = 1.0
 
-draw_dot(o).render('graph', format='svg')
+# draw_dot(o).render('graph', format='svg')
+
+# Tensor
+# x1 = torch.tensor([2.0], dtype=torch.double, requires_grad=True)
+# x2 = torch.tensor([0.0], dtype=torch.double, requires_grad=True)
+
+# w1 = torch.tensor([-3.0], dtype=torch.double, requires_grad=True)
+# w2 = torch.tensor([1.0], dtype=torch.double, requires_grad=True)
+# b = torch.tensor([6.8813735870195432], dtype=torch.double, requires_grad=True)
+# n = x1 * w1 +x2 *w2 + b
+# o = torch.tanh(n)
+# print(o.data.item())
+# o.backward()
+# print('---')
+# print('x1', x1.grad.item())
+# print('w1', w1.grad.item())
+# print('x2', x2.grad.item())
+# print('w2', w2.grad.item())
+
+class Neuron:
+	def __init__(self, nin):
+		self.w = [Value(random.uniform(-1, 1)) for _ in range(nin)]
+		self.b = Value(random.uniform(-1, 1))
+	
+	def __call__(self, x):
+		act = sum((wi * xi for wi, xi in zip(self.w, x)), self.b)
+		out = act.tanh()
+		return out
+
+class Layer:
+	def __init__(self, nin, nout):
+		self.neurons = [Neuron(nin) for _ in range(nout)]
+
+	def __call__(self, x):
+		outs = [n(x) for n in self.neurons]
+		return outs[0] if len(outs) == 1 else outs
+
+class MLP:
+	def __init__(self, nin, nouts):
+		sz = [nin] + nouts
+		self.layers = [Layer(sz[i], sz[i + 1]) for i in range(len(nouts))]
+	
+	def __call__(self, x):
+		for layer in self.layers:
+			x = layer(x)
+		return x
+
+# Single neuron
+# x = [2.0, 3.0]
+# n = Neuron(2)
+# print(n(x))
+
+#Layer of 3 neurons
+# x = [2.0, 3.0]
+# n = Layer(2, 3)
+# print(n(x))
+
+# Réseau de neurones 3 4 4 1
+x = [2.0, 3.0, -1]
+n = MLP(3, [4, 4, 1])
+print(n(x))
+draw_dot(n(x)).render('graph', format='svg')
