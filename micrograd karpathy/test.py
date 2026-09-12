a790ff175283c815eb096a8230b724c6664f9c7e
mAgAtSuRi <tristan.latuilliere@gmail.com>
2026-09-12 13:58:07 +0200

    neural network training finished

--- micrograd karpathy/test.py
+++ micrograd karpathy/test.py
@@ -189,6 +189,9 @@ class Neuron:
 		out = act.tanh()
 		return out
 
+	def parameters(self):
+		return self.w + [self.b]
+
 class Layer:
 	def __init__(self, nin, nout):
 		self.neurons = [Neuron(nin) for _ in range(nout)]
@@ -197,6 +200,8 @@ class Layer:
 		outs = [n(x) for n in self.neurons]
 		return outs[0] if len(outs) == 1 else outs
 
+	def parameters(self):
+		return [p for neuron in self.neurons for p in neuron.parameters]
 class MLP:
 	def __init__(self, nin, nouts):
 		sz = [nin] + nouts
@@ -206,6 +211,9 @@ class MLP:
 		for layer in self.layers:
 			x = layer(x)
 		return x
+
+	def parameters(self):
+		return [p for layer in self.layers for p in layer.parameters]
 
 # Single neuron
 # x = [2.0, 3.0]
@@ -220,5 +228,40 @@ class MLP:
 # Réseau de neurones 3 4 4 1
 x = [2.0, 3.0, -1]
 n = MLP(3, [4, 4, 1])
-print(n(x))
-draw_dot(n(x)).render('graph', format='svg')
+# print(n(x))
+# draw_dot(n(x)).render('graph', format='svg')
+
+# print(n.parameters())
+
+# dataset
+xs = [
+	[2.0, 3.0, -1.0],
+	[3.0, -1.0, 0.5],
+	[0.5, 1.0, 1.0],
+	[1.0, 1.0, -1.0],
+]
+ys = [1.0, -1.0, -1.0, 1.0] #desired targets
+# ypred = [n(x) for x in xs]
+# print(ypred)
+
+# Loss 
+# loss = sum((yout - ygt)**2 for yout, ygt in zip(ypred, ys))
+# print(loss)
+
+# Training Neural Network 
+
+# forward pass
+for k in range(10):
+	ypred = [n(x) for x in xs]
+	loss = sum((yout - ygt)**2 for ygt, yout in zip(ys, ypred))
+
+	# backward pass
+	for p in n.parameters:
+		p.grad = 0.0
+	loss.backward()
+
+	# update 
+	for p in n.parameters():
+		p.data += -0.05 * p.grad
+	
+	print(k, loss.data)
